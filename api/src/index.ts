f7d1491001fd01882bcb66c741fcbce9dbfd512b
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-08 19:03:06 +0200

    feat: restore split file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,60 @@
 import swagger from "@elysiajs/swagger";
+import { ExhibitionController } from "#controllers/exhibition.controller";
 import { UserController } from "#controllers/user.controller";
 import { Elysia } from "elysia";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
 const app = new Elysia()
-    .use(swagger({ path: "/docs" }))
+    .use(
+        swagger({
+            path: "/docs",
+            documentation: {
+                info: {
+                    title: "Artway API Documentation",
+                    version: "ALPHA",
+                    description:
+                        "API documentation for <a href='https://getartway.app'>Artway</a>.",
+                },
+            },
+        }),
+    )
     .get("/", ({ redirect }) => redirect("/docs"), {
         detail: {
             hide: true,
         },
     })
-    .group("/api", (app) => app.use(UserController))
-    .listen(Bun.env.API_PORT);
+    .group("/api", (app) =>
+        app
+            .use(UserController)
+            .use(ExhibitionController)
+            .group("/medias", (app) =>
+                app.get("/:id/", async ({ params, set }) => {
+                    const { id } = params;
+
+                    try {
+                        const filePath = join(
+                            process.cwd(),
+                            "medias",
+                            `${id}.webp`,
+                        );
+                        const file = await readFile(filePath);
+
+                        set.headers["Content-Type"] = "image/webp";
+                        return new Response(file);
+                    } catch (e) {
+                        set.status = 404;
+                        return {
+                            error: "File not found",
+                        };
+                    }
+                }),
+            ),
+    )
+    .listen(Number(Bun.env.API_PORT) || 3000);
 
 console.log(
     `API is running @ ${app.server?.url}\n Docs are available @ ${app.server?.url}docs/`,
 );
+
+export type App = typeof app;
