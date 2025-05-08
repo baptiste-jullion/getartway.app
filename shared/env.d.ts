b7071315ea3808c5f630b2d3b9af0a1d1d245571
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-08 18:55:01 +0200

    chore: enhance .env type declaration

---
+++ shared/env.d.ts
@@ -0,0 +1,21 @@
+declare module "bun" {
+    interface Env<
+        API_PROTOCOL extends "http" | "https",
+        API_PORT extends number,
+        API_HOST extends `${number}.${number}.${number}.${number}`,
+    > {
+        DB_HOST: string;
+        DB_PORT: number;
+        DB_NAME: string;
+        DB_USER: string;
+        DB_PASSWORD: string;
+
+        DB_URL: string;
+        SHADOW_DATABASE_URL: string;
+
+        API_PROTOCOL: API_PROTOCOL;
+        API_PORT: API_PORT;
+        API_HOST: API_HOST;
+        API_URL: `${API_PROTOCOL}://${API_HOST}:${API_PORT}`;
+    }
+}
