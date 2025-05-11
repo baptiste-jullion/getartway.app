a8f3f9cdc234ab6c03fd1b6b6f596aba395f1a57
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-11 16:42:58 +0200

    refactor: update fixtures

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,12 +1,19 @@
 import { db } from "#database";
-import { Artist, ArtistCategory, Category, Exhibition } from "#models";
+import {
+    Artist,
+    ArtistCategory,
+    Category,
+    Exhibition,
+    Location,
+} from "#models";
 import * as yaml from "js-yaml";
 import * as fs from "node:fs";
 
 interface FakeData {
+    locations: Location[];
     artists: Artist[];
     categories: Category[];
-    exhibitions: (Exhibition & { location: [number, number] })[];
+    exhibitions: Exhibition[];
     artist_categories: ArtistCategory[];
 }
 
@@ -18,17 +25,12 @@ async function main() {
     await db.artistCategory.deleteMany({});
     await db.exhibition.deleteMany({});
     await db.artist.deleteMany({});
+    await db.location.deleteMany({});
     await db.category.deleteMany({});
 
-    const locations: Record<string, [number, number]> = {};
-    for (const exhibition of data.exhibitions) {
-        exhibition.startDate = new Date(exhibition.startDate);
-        exhibition.endDate = new Date(exhibition.endDate);
-
-        locations[exhibition.id] = exhibition.location;
-        // @ts-expect-error Location will be set later cause Prisma doesn't support this type
-        delete exhibition.location;
-    }
+    await db.location.createMany({
+        data: data.locations,
+    });
 
     await db.artist.createMany({
         data: data.artists,
@@ -41,17 +43,6 @@ async function main() {
     await db.exhibition.createMany({
         data: data.exhibitions,
     });
-
-    for (const exhibitionId of Object.keys(locations)) {
-        await db.$executeRaw`
-            UPDATE "Exhibition"
-            SET location = ST_SetSRID(
-                    ST_MakePoint(${locations[exhibitionId][0]}, ${locations[exhibitionId][1]}),
-                    4326
-                           )
-            WHERE id = ${exhibitionId}::uuid
-        `;
-    }
 
     await db.artistCategory.createMany({
         data: data.artist_categories,
