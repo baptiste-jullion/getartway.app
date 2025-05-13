c8a716ef21318c447d9262e2fb8e77ddfff706b1
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-14 00:07:23 +0200

    feat: add constants

---
+++ mobile/src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const LocationsConstants: Record<
+    "ANNECY" | "ARA_MAX_NE" | "ARA_MAX_SW",
+    [number, number]
+> = {
+    ANNECY: [6.128158, 45.899247],
+    ARA_MAX_NE: [7.18556 + 0.25, 46.80417 + 0.25],
+    ARA_MAX_SW: [2.06278 - 0.25, 44.11556 - 0.25],
+};
