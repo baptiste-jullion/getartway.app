8c09512f65011767eb09eb85fd2adae532a0710b
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-14 23:59:38 +0200

    refactor: remove useless code

--- mobile/src/components/Map/UserLocation.tsx
+++ mobile/src/components/Map/UserLocation.tsx
@@ -5,42 +5,6 @@ import React from "react";
 export const UserLocation = () => {
     const { setUserLocation } = useMapContext();
 
-    // const [hasLocationPermission, setHasLocationPermission] = useState(false);
-    //
-    // useEffect(() => {
-    //     const requestLocationPermission = async () => {
-    //         if (Platform.OS === "android") {
-    //             try {
-    //                 const granted = await PermissionsAndroid.request(
-    //                     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-    //                     {
-    //                         title: "Location Permission",
-    //                         message:
-    //                             "This app needs location permission to show your location on the map.",
-    //                         buttonNeutral: "Ask Me Later",
-    //                         buttonNegative: "Cancel",
-    //                         buttonPositive: "OK",
-    //                     },
-    //                 );
-    //                 if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-    //                     console.log("You can use the location");
-    //                     setHasLocationPermission(true);
-    //                 } else {
-    //                     console.log("Location permission denied");
-    //                 }
-    //             } catch (err) {
-    //                 console.warn(err);
-    //             }
-    //         } else {
-    //             // For iOS, you'll need to handle location permissions differently
-    //             // using the react-native-permissions library or similar.
-    //             setHasLocationPermission(true); // Assume permission is granted for simplicity in this example
-    //         }
-    //     };
-    //
-    //     requestLocationPermission();
-    // }, []);
-
     return (
         <>
             {
