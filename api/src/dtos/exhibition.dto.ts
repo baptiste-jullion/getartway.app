26bde39296765d877c83224e47d9f89c4f50128c
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-15 00:10:01 +0200

    refactor: minor tweaks on dto & type

--- api/src/dtos/exhibition.dto.ts
+++ api/src/dtos/exhibition.dto.ts
@@ -1,35 +1,38 @@
-import { Timestamps } from "#utils";
 import { t } from "elysia";
 
-const ExhibitionDTO = t.Intersect([
-    t.Object({
-        id: t.String({ format: "uuid" }),
-        title: t.String({ maxLength: 255 }),
-        description: t.String(),
-        startDate: t.Date({
-            examples: [new Date()],
-        }),
-        endDate: t.Date({
-            examples: [new Date()],
-        }),
-        cover: t.String(),
-        price: t.Number(),
-        location: t.Object({
-            id: t.String({ format: "uuid" }),
-            lat: t.Number({ format: "float", examples: [45.900002] }),
-            lng: t.Number({ format: "float", examples: [6.1252859] }),
-        }),
-        isReviewed: t.Boolean({ default: false }),
-        website: t.String(),
-
-        category: t.Object({
-            id: t.String({ format: "uuid" }),
-            label: t.String(),
-            description: t.String(),
-        }),
+const ExhibitionDTO = t.Object({
+    id: t.String({ format: "uuid" }),
+    title: t.String({ maxLength: 255 }),
+    description: t.String(),
+    startDate: t.Date({
+        examples: [new Date()],
     }),
-    Timestamps,
-]);
+    endDate: t.Date({
+        examples: [new Date()],
+    }),
+    cover: t.String(),
+    price: t.Number(),
+    location: t.Object({
+        id: t.String({ format: "uuid" }),
+        lat: t.Number({ format: "float", examples: [45.900002] }),
+        lng: t.Number({ format: "float", examples: [6.1252859] }),
+    }),
+    isReviewed: t.Boolean({ default: false }),
+    website: t.String(),
+
+    category: t.Object({
+        id: t.String({ format: "uuid" }),
+        label: t.String(),
+        description: t.String(),
+    }),
+
+    createdAt: t.Date({
+        examples: [new Date()],
+    }),
+    updatedAt: t.Date({
+        examples: [new Date()],
+    }),
+});
 
 export type TExhibition = typeof ExhibitionDTO.static;
 
@@ -39,3 +42,5 @@ export const ExhibitionListDTO = t.Pick(ExhibitionDTO, [
     "cover",
     "location",
 ]);
+
+export type TExhibitionList = typeof ExhibitionListDTO.static;
