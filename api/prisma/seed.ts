f8f0eb2e2f2dfcc45ef5cfc1710b3cdb825bac40
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-16 19:13:12 +0200

    feat: add new fixtures

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -2,6 +2,7 @@ import { db } from "#database";
 import {
     Artist,
     ArtistCategory,
+    ArtistExhibition,
     Category,
     Exhibition,
     Location,
@@ -12,21 +13,35 @@ import * as fs from "node:fs";
 interface FakeData {
     locations: Location[];
     artists: Artist[];
-    categories: Category[];
     exhibitions: Exhibition[];
     artist_categories: ArtistCategory[];
+    artist_exhibitions: ArtistExhibition[];
+}
+
+interface InitialData {
+    categories: Category[];
 }
 
 async function main() {
     const file = fs.readFileSync(`${__dirname}/fake-data.yaml`, "utf8");
     const data = yaml.load(file) as FakeData;
+    const initialDataFile = fs.readFileSync(
+        `${__dirname}/initial-data.yaml`,
+        "utf8",
+    );
+    const initialData = yaml.load(initialDataFile) as InitialData;
 
     // drop all existing data
     await db.artistCategory.deleteMany({});
+    await db.artistExhibition.deleteMany({});
     await db.exhibition.deleteMany({});
+    await db.category.deleteMany({});
     await db.artist.deleteMany({});
     await db.location.deleteMany({});
-    await db.category.deleteMany({});
+
+    await db.category.createMany({
+        data: initialData.categories,
+    });
 
     await db.location.createMany({
         data: data.locations,
@@ -36,16 +51,16 @@ async function main() {
         data: data.artists,
     });
 
-    await db.category.createMany({
-        data: data.categories,
-    });
-
     await db.exhibition.createMany({
         data: data.exhibitions,
     });
 
     await db.artistCategory.createMany({
         data: data.artist_categories,
+    });
+
+    await db.artistExhibition.createMany({
+        data: data.artist_exhibitions,
     });
 }
 
