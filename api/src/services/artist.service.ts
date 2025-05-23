5d47622ba957d5f3864db8837d5880eec81d7d7f
Baptiste <contact@baptiste-jullion.dev>
2025-05-23 14:49:13 +0200

    feat: enhance artist listing to include cover image URL

--- api/src/services/artist.service.ts
+++ api/src/services/artist.service.ts
@@ -2,6 +2,11 @@ import { db } from "#database";
 
 export class ArtistService {
     static async list() {
-        return await db.artist.findMany();
+        const results = await db.artist.findMany();
+
+        return results.map((artist) => ({
+            ...artist,
+            cover: `${Bun.env.API_URL}/api/medias/${artist.image}`,
+        }));
     }
 }
