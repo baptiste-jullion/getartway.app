ec664e67a4b718462373b89558d5bde20424da03
Baptiste <contact@baptiste-jullion.dev>
2025-05-23 13:37:37 +0200

    feat: initialize Nuxt 3 project with Tailwind CSS and essential files
    - Added nuxt.config.ts for Nuxt configuration with Tailwind CSS integration.
    - Created package.json with scripts for building, developing, and generating the app.
    - Added favicon.ico for branding.
    - Included robots.txt to manage web crawler access.
    - Added user-guide.pdf for user documentation.
    - Created tsconfig.json files for TypeScript support in server and project.

---
+++ website/app.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="flex h-dvh items-center justify-center p-8">
+        <div>
+            <LogoBaseLine class="w-full" />
+        </div>
+        <footer
+            class="fixed bottom-0 p-8 font-medium flex w-full flex-wrap justify-between gap-4"
+        >
+            <a
+                href="/user-guide.pdf"
+                target="_blank"
+                class="flex items-center gap-2"
+            >
+                <IconGuide class="size-6" />
+                <span>Guide utilisateur</span>
+            </a>
+            <a
+                href="https://github.com/baptiste-jullion/getartway.app"
+                target="_blank"
+                class="flex items-center gap-2"
+            >
+                <IconGithub class="size-6" />
+                <span>Code Source</span>
+            </a>
+        </footer>
+    </section>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+    title: "Artway | Explorez l'art qui vous entoure",
+});
+</script>
