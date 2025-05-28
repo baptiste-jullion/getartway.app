52bdb70a09da37d9e13abeb4d44604f178ca37f9
Baptiste <contact@baptiste-jullion.dev>
2025-05-28 21:36:08 +0200

    feat: add Google Fonts module and update CSS variables for icons

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -3,8 +3,21 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2025-05-15",
     devtools: { enabled: true },
+
     vite: {
         plugins: [tailwindcss()],
     },
+
     css: ["~/assets/css/main.css"],
+    modules: [
+        [
+            "@nuxtjs/google-fonts",
+            {
+                families: {
+                    "Funnel Display": true,
+                    "DM Sans": true,
+                },
+            },
+        ],
+    ],
 });
